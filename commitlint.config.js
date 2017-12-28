e66a5cd03a9c2a164ecb8c43ff8fbac63d03914f
TaylorPzreal <taylorpzreal@gmail.com>
2017-12-28 11:15:59 +0800

    update: Commitlint
    add new subject-case conf

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: [
-    {
-      'type-enum': [0, 'always', ['add', 'update', 'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
-    },
-  ],
+  rules: {
+    'type-enum': [2, 'always', ['add', 'update', 'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
+    'subject-case': [2, 'always', ['lower-case', 'sentence-case']],
+  },
 };
