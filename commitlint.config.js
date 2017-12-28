a9feb592bb7de6bff93d24c23a57bf06bb2b5007
TaylorPzreal <taylorpzreal@gmail.com>
2017-12-28 19:29:42 +0800

    fix: jest test
    make ReactDom to ReactDOM and keep same, update eslint conf

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', ['add', 'update', 'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
-    'subject-case': [0, 'never', ['lower-case']],
-  },
+    'subject-case': [0, 'never', ['lower-case']]
+  }
 };
